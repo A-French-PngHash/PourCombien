2ebbe12704c88eeb65bfc3eecafcdbdff44aa59f
Titouan-cpu <titouan.blossier@orange.Fr>
2019-11-16 10:35:58 +0100

    Initial Commit

---
+++ PourCombienTu/ViewController.swift
@@ -0,0 +1,14 @@
+////This file was created on 16/11/2019 by Titouan Blossier. Do not reproduce without permission.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ PourCombienTuTests/PourCombienTuTests.swift
@@ -0,0 +1,28 @@
+////This file was created on 16/11/2019 by Titouan Blossier. Do not reproduce without permission.
+
+import XCTest
+@testable import PourCombienTu
+
+class PourCombienTuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
